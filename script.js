0af8fe3d31707246d8e277c40f56ec3ccb383685
JOAO  LUIZ MARCAL <0220832311001@fatectq.local>
2023-11-21 10:41:11 -0300

    Initial commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ new window.VLibras.Widget({
     rootPah: '/app',
     personalization: 'https://vlibras.gov.br/config/default_logo.json',
     opacity: 0.5,
-    position: 'L',
+    position: 'R',
     avatar: 'random',
   });
 
